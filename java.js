285f3aed4c29404bd441ae1e5cb7107c402e78fc
Antonio <antoniogil105@gmail.com>
2020-02-27 15:21:22 -0700

    Started development on functionality
    Researched and worked with Mr. B on how to set up a column and row counter/hover.

--- java.js
+++ java.js
@@ -4,7 +4,32 @@ var position = [
     [ , , , , , , ],
     [ , , , , , , ],
     [ , , , , , , ],
-    [ , , , , , , ],
+    [ , , , , , , ]
 ];
+var currentID;
+var currentPositionX;
+var currentPositionY;
+function tableClick()
+{
+    document.getElementById(currentID).style.backgroundColor="red";
+/*  player = true
+    for(var count = currentPositionX; count <= 7; count++)
+    {
+        if(document.getElementById(currentID[8]).name == "empty")
+        {
+            document.getElementById(currentID[8]).style.backgroundColor="red";
+            document.getElementById(currentID).name = "filled";
+        }
+    }
+*/
+}
+
+function game(x)
+{
+    currentID = x.id;
+    currentPositionX = currentID[3];
+    currentPositionY = currentID[8]; 
+}
+
 
 
