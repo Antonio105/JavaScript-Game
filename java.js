e97c6b6f1a9673f53f8e6e83d8c438223ecbc1b6
Antonio <antoniogil105@gmail.com>
2020-03-02 15:23:38 -0700

    Worked on functionality
    Developed a system that will make sure a used space will be set to filled.

--- java.js
+++ java.js
@@ -6,33 +6,36 @@ var position = [
     [ , , , , , , ],
     [ , , , , , , ]
 ];
+
+for(var j= 1;j <=6;j++)
+{
+    for(var k=1;k<=7;k++){
+    var tek = "row"+j+"-col"+k;
+    document.getElementById(tek).name = "empty";
+    }
+}
+
 var currentID;
 var currentPositionX;
 var currentPositionY;
-function tableClick()
-{
-    document.getElementById(currentID).style.backgroundColor="red";
-/*  player = true
-    for(var count = currentPositionX; count <= 7; count++)
-    {
-        if(document.getElementById(currentID[8]).name == "empty")
-        {
-            document.getElementById(currentID[8]).style.backgroundColor="red";
-            document.getElementById(currentID).name = "filled";
-        }
-    }
-*/
-}
 
 function game(x)
 {
     currentID = x.id;
     currentPositionX = currentID[3];
     currentPositionY = currentID[8]; 
-    if(position[0],[0]){
-        
-        document.getElementById("one").style.background = "red";
-    }
 }
+
+function tableClick()
+{
+        if(document.getElementById(currentID).name == "empty")
+        {
+            document.getElementById(currentID).style.backgroundColor="red";
+            document.getElementById(currentID).name = "filled";
+            console.log(document.getElementById(currentID).name); 
+        }
+}
+
+
 
 
