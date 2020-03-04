1934edd93e679a5f6d83203b549802a1fec917d5
Antonio <antoniogil105@gmail.com>
2020-03-04 15:57:21 -0700

    Developed further functionality
    Me and Wulff have been working on these commits from my computer. We had Mr. B help us develop our stacking and winner.

--- java.js
+++ java.js
@@ -6,7 +6,29 @@ var position = [
     [ , , , , , , ],
     [ , , , , , , ]
 ];
-
+window.onload=function board()
+{
+    for(var j = 1; j<=6;j++)
+    {
+        for(var k =1;k<=7;k++)
+        {
+            var filler = "row"+j+"-col"+k;
+            document.getElementById(filler).name="empty";
+        }
+    }
+}
+function boboard()
+{
+    for(var j = 1; j<=6;j++)
+    {
+        for(var k =1;k<=7;k++)
+        {
+            var filler = "row"+j+"-col"+k;
+            console.log(filler);
+            console.log(document.getElementById(filler).name);
+        }
+    }
+}
 var currentID;
 var currentPositionX;
 var currentPositionY;
@@ -22,51 +44,144 @@ var player = true;
 function tableClick()
 {
     var tcpx = currentPositionX;
-        for(var count = 1; count <=6; count++)
-        {
-            if(document.getElementById(currentID).name=="empty")
+    var marker = "row"+tcpx+"-col"+currentPositionY;
+        for(var count = 0; count <=5; count++)
+        { 
+            if(tcpx==6 && document.getElementById(marker).name == "empty")
             {
-                tcpx+=1;
-                var marker = "row"+tcpx+"-col"+currentPositionY;
-                if(document.getElementById(marker).name=="filled")
-                    {
-                        if(player == true)
-                        {
-                            document.getElementById(currentID).style.backgroundColor="red";
-                            document.getElementById(currentID).name = "filled";
-                            console.log(document.getElementById(currentID).name);
-                            player = false;
-                        }
-                        else if(player == false)
-                        {
-                            document.getElementById(currentID).style.backgroundColor="yellow";
-                            document.getElementById(currentID).name = "filled";
-                            console.log(document.getElementById(currentID).name);
-                            player = true;  
-                        }
-                    }
-                else
-                    {
-                        tcpx = 6;
-                        var marker = "row"+tcpx+"-col"+currentPositionY;
-                        if(document.getElementById(marker).name=="empty")
-                        {
+                var marker = "row"+6+"-col"+currentPositionY;
                             if(player == true)
                             {
-                                document.getElementById(currentID).style.backgroundColor="red";
-                                document.getElementById(currentID).name = "filled";
-                                console.log(document.getElementById(currentID).name);
+                                document.getElementById(marker).style.backgroundColor="red";
+                                document.getElementById(marker).name = "filled";
                                 player = false;
+                                break;
+                            }
+    
+
+                            else if(player == false)
+                            {
+                                document.getElementById(marker).style.backgroundColor="yellow";
+                                document.getElementById(marker).name = "filled";
+                                player = true;  
+                                break;  
+                            }
+
+            }
+            else if(document.getElementById(marker).name=="empty")
+            {
+                tcpx= parseInt(tcpx) +1;
+            }
+/*
+            else if (document.getElementById(currentID).name=="empty")
+            {
+                tcpx+=1;
+                marker="row"+tcpx+"-col"+currentPositionY; 
+                if(document.getElementById(marker).name=="filled")
+                {
+                    tcpx-=1;
+                    marker="row"+tcpx+"-col"+currentPositionY; 
+                    console.log(tcpx);
+                    console.log("k");
+                    if(player == true)
+                            {
+                                document.getElementById(marker).style.backgroundColor="red";
+                                document.getElementById(marker).name = "filled";
+                                player = false;
+                                break;
                             }
                             else if(player == false)
                             {
-                                document.getElementById(currentID).style.backgroundColor="yellow";
-                                document.getElementById(currentID).name = "filled";
-                                console.log(document.getElementById(currentID).name);
+                                document.getElementById(marker).style.backgroundColor="yellow";
+                                document.getElementById(marker).name = "filled";
                                 player = true;  
+                                break;
                             }
-                        }
-                    }
+                }
             }
-        }   
+*/
+            else if(document.getElementById(currentID).name=="filled")
+            {
+                tcpx-=1;
+                marker="row"+tcpx+"-col"+currentPositionY;
+                if(document.getElementById(marker).name=="empty")
+                {
+                    if(player == true)
+                            {
+                                document.getElementById(marker).style.backgroundColor="red";
+                                document.getElementById(marker).name = "filled";
+                                player = false;
+                                break;
+                            }
+                            else if(player == false)
+                            {
+                                document.getElementById(marker).style.backgroundColor="yellow";
+                                document.getElementById(marker).name = "filled";
+                                player = true;  
+                                break;
+                            }
+                }
+            }
+            /*
+            boboard();
+            */
+        }
 }
+var rred =0;
+var yyellow=0;
+var plate;
+
+function checkWinner()
+{
+    for(var w = 1; w <=6; w++)
+    {  
+        for(var q = 1; q<=7;q++)
+        {
+            var teller ="row"+w+"-col"+q;
+            var hold = document.getElementById(teller).style.backgroundColor;
+            if(hold=="red")
+            {  
+                plate = true;
+                if(plate==true)
+                {
+                    console.log("red");
+                    console.log(rred);
+                    rred+=1;
+                if(rred==4)
+                {
+                    alert("Winner! R")
+                    break;
+                }
+                    break;
+                }
+                else{
+                    rred=0;
+                    break;
+                }
+            }
+            else if(hold=="yellow")
+            {
+                plate = false;
+                if(plate==false)
+                {
+                    console.log("yellow");
+                    console.log(yyellow);
+                    yyellow+=1;
+                    if(yyellow==4)
+                    {
+                        alert("Winner! Y")
+                        break;
+                    }
+                        break;
+                    }
+                }
+                else
+                {
+                    yyellow=0;
+                    break;
+                }
+            }
+        }
+ }
+
+
